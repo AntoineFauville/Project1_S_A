4990722eed4093020d742769dc71cdbb4cf1b26f
sheido <af8081@gmail.com>
2018-03-24 17:26:31 +0100

    base game part 1 : 3 buttons  + dying part

---
+++ Project/Project1/Assets/SpawnController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour {
+
+	public GameObject[] Unit;
+	public GameObject[] SpawnPoints;
+
+	public void OnClic(int unitIndex){
+		Instantiate (Unit[unitIndex],SpawnPoints[unitIndex].transform);
+	}
+}
---
+++ Project/Project1/Assets/UnitController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour {
+
+	public int health;
+	public float distance;
+	float distanceMin = 1.5f;
+
+	void Start () {
+		health = 5;
+		StartCoroutine (WaitHealthCalculation());
+	}
+	
+
+	void Update () {
+
+		if(distance > distanceMin){
+			transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(4,0));
+		}
+
+		distance = Vector3.Distance (transform.position, GameObject.Find ("Wall").transform.position);
+		//print (distance);
+
+		if (health <= 0) {
+			GameObject.DestroyImmediate (this.gameObject);
+		}
+	}
+
+	IEnumerator WaitHealthCalculation(){
+		yield return new WaitForSeconds (0.5f);
+		if (distance <= distanceMin) {
+			health -= 1;
+			transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(-100,200));
+		}
+		StartCoroutine (WaitHealthCalculation());
+	}
+}
